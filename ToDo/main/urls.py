970ae5938334699d6b00f0f914125f57e472d379
Rustam <139070458+rustamovg@users.noreply.github.com>
2023-07-10 13:37:19 +0500

    Add files via upload

---
+++ ToDo/main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+app_name = 'main'
+
+urlpatterns = [
+    path('<int:pk>/', DetailTodo.as_view()),
+    path('', ListTodo.as_view()),
+    path('create', CreateTodo.as_view()),
+    path('delete/<int:pk>', DeleteTodo.as_view())
+]
